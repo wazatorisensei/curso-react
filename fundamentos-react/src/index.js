891297f1be7c26267e770afefacfcf229664aa43
Jan Yuzo Kohara <janyuzokohara@gmail.com>
2022-07-08 11:04:21 -0300

    first commit

---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import Primeiro from './components/basicos/Primeiro'
+
+//const tag = <strong>Olá React!</strong>
+
+ReactDOM.render(
+    <div>
+        <Primeiro></Primeiro>
+    </div>,
+    document.getElementById('root')
+)

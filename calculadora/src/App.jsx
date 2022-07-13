cd8d5e5ade8db2e0001afc453e59eea01f76652f
Jan Yuzo Kohara <janyuzokohara@gmail.com>
2022-07-13 19:29:07 -0300

    calculator project implements

---
+++ calculadora/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+import Calculator from './main/Calculator'
+
+export default () => (
+
+    <div>
+        <h1>Calculadora</h1>
+        <Calculator />
+    </div>
+
+
+)
